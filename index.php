39a0854d4ee277fd93a04f1d94a65fffe24acd10
Name Surname <email@example.com>
2017-01-19 16:55:04 +0200

    changed whole index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<html>
-  <body>
-    <h1>
-      Testuojam
-      naujas
-      naujas
-      dar vienas naujas
-    </h1>
-  </body>
-</html>
+<?php
+Class User
+{
+    protected $userid;
+    public function __construct($id)
+    {
+        $this->userid = $id;
+    }
+    public function getUserId()
+    {
+        return $this->userid;
+    }
+    public function setUserId($id)
+    {
+        $this->userid = $id;
+    }
+}
+$user = new User(15);
+echo $user->getUserId;
+?>
